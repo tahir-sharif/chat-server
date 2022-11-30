b14f66b99df518ac9c6ddab327c77fb2c1127131
tahir150 <tahirtv.islam@gmail.com>
2022-11-30 13:01:59 +0500

    port added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/db");
 var cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 6500;
 
 // middlewares
 app.use(express.json());
@@ -11,24 +11,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use((req, res, next) => {
-  console.log(req.method, req.url, req.body)
-  next()
-})
+  console.log(req.method, req.url, req.body);
+  next();
+});
 
 // Api Requests
 app.get("/", (req, res) => {
   res.send("Hello Tahir !");
 });
 
-app.use(
-  "/api/user",
-  require("./routes/userRoutes")
-);
+app.use("/api/user", require("./routes/userRoutes"));
 
-app.use(
-  "/api/chats",
-  require("./routes/chatRoutes")
-);
+app.use("/api/chats", require("./routes/chatRoutes"));
 
 //--Connecting Database and Listening Server
 connectDB();
